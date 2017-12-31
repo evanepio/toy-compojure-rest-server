80361e17480a948169cf6ff76492d8cf81a019cd
Evan Porter <evan.porter@dotman.ca>
2017-12-31 02:14:19 -0400

    update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject toy-compojure-rest-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A toy compojure rest server"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
